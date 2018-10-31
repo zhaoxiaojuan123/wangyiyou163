cb0d1ec90616f49bb0e7640a445d5c7bfcf5451e
chenyan316 <2824842400@qq.com>
2018-10-31 15:33:45 +0800

    end

--- wangyiyou163/src/container/Personal/Reg/RegInput.js
+++ wangyiyou163/src/container/Personal/Reg/RegInput.js
@@ -1,21 +1,26 @@
 import React from "react";
 import './index.less';
 export default class RegInput extends React.Component{
+    handleReg=()=>{
+        let username=this.username.value;
+        let password=this.password.value;
+        this.props.reg({username,password});
+    }
     render(){
         return <div className="inputbox">
             <div className="u-input">
-                <input type="text" placeholder="请输入手机号"></input>
+                <input type="text" placeholder="请输入手机号" ref={x=>{this.username=x}}></input>
                 <input type="text" placeholder="请输入短信验证码">
                 
                 </input>
                 <div className="pcbtn f-f1">
                     <a href="" className="tabfocus getsmscode">获取验证码</a>
                 </div>
-                <input type="text" placeholder="请输入密码">
+                <input type="text" placeholder="请输入密码"  ref={x=>{this.password=x}}>
                 
                 </input>
                 <div className="f-cb loginbox">
-                    <a className="u-loginbtn">注册</a>
+                    <a className="u-loginbtn" onClick={this.handleReg}>注册</a>
                 </div>
             </div>
             <div className="fur-item fur-agree">  
